e332da54ef95df1ab4d74c76b0c88c8e6914515e
William Durand <will+git@drnd.me>
2017-05-18 20:22:55 +0200

    Jumping headlong into long mode

--- boot.asm
+++ boot.asm
@@ -44,22 +44,17 @@ start:
     or eax, 1 << 16
     mov cr0, eax
 
-	;   size place      thing
-	;   |    |          |
-	;   V    V          V
-	mov word [0xb8000], 0x0248 ; H
-    mov word [0xb8002], 0x0265 ; e
-    mov word [0xb8004], 0x026c ; l
-    mov word [0xb8006], 0x026c ; l
-    mov word [0xb8008], 0x026f ; o
-    mov word [0xb800a], 0x022c ; ,
-    mov word [0xb800c], 0x0220 ;
-    mov word [0xb800e], 0x0277 ; w
-    mov word [0xb8010], 0x026f ; o
-    mov word [0xb8012], 0x0272 ; r
-    mov word [0xb8014], 0x026c ; l
-    mov word [0xb8016], 0x0264 ; d
-    mov word [0xb8018], 0x0221 ; !
+    lgdt [gdt64.pointer]
+
+	; update selectors
+	mov ax, gdt64.data
+	mov ss, ax
+	mov ds, ax
+	mov es, ax
+
+	; jump to long mode!
+	jmp gdt64.code:long_mode_start
+
     hlt
 
 ; block started by symbol
@@ -85,3 +80,15 @@ gdt64:
 
 .data: equ $ - gdt64
     dq (1<<44) | (1<<47) | (1<<41)
+
+.pointer:
+    dw .pointer - gdt64 - 1
+    dq gdt64
+
+section .text
+bits 64
+long_mode_start:
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+
+    hlt
