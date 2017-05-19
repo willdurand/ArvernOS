1c09dc64e454a35767e96e1e3b09e8b7ab1d0053
William Durand <will+git@drnd.me>
2017-05-19 14:12:46 +0200

    Use define in asm code

--- asm/multiboot_header.asm
+++ asm/multiboot_header.asm
@@ -1,11 +1,14 @@
 ; cf. https://intermezzos.github.io/book/multiboot-headers.html
 section .multiboot_header
 
+%define MULTIBOOT_HEADER_MAGIC 0xe85250d6
+%define MULTIBOOT_HEADER_FLAGS 0
+
 ; it is a label
 header_start:
 	; `dd` means 'define double word'
-	dd 0xe85250d6                ; magic number
-	dd 0                         ; protected mode code
+	dd MULTIBOOT_HEADER_MAGIC
+	dd MULTIBOOT_HEADER_FLAGS
 	dd header_end - header_start ; header length
 
     ; checksum
