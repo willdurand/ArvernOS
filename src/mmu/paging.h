ffc1c10e1ec8a337a0c5d10e2ac218070d8d185e
William Durand <will+git@drnd.me>
2017-05-23 01:06:43 +0200

    Fix page table entry struct

--- src/mmu/paging.h
+++ src/mmu/paging.h
@@ -11,17 +11,36 @@
 typedef uint64_t virtual_address_t;
 typedef uint64_t page_t;
 
+// cf.http://os.phil-opp.com/modifying-page-tables.html#page-table-entries
+// cf. https://github.com/tmathmeyer/sos
 typedef union page_entry {
     uint64_t packed;
     struct {
-        uint8_t present: 1,
-                writable: 1,
-                user_accessible: 1,
-                write_through : 1,
-                no_cache : 1,
-                accessed : 1,
-                dirty : 1,
-                huge_page : 1;
+        uint8_t present : 1;
+        uint8_t writable : 1;
+        uint8_t user_accessable : 1;
+        uint8_t write_thru_cache : 1;
+        uint8_t disable_cache : 1;
+        uint8_t accessed : 1;
+        uint8_t dirty : 1;
+        uint8_t huge_page : 1;
+        uint8_t global : 1;
+        uint8_t OS_1 : 1;
+        uint8_t OS_2 : 1;
+        uint8_t OS_3 : 1;
+        uint64_t _addr_mask : 40;
+        uint8_t OS_4 : 1;
+        uint8_t OS_5 : 1;
+        uint8_t OS_6 : 1;
+        uint8_t OS_7 : 1;
+        uint8_t OS_8 : 1;
+        uint8_t OS_9 : 1;
+        uint8_t OS_A : 1;
+        uint8_t OS_B : 1;
+        uint8_t OS_C : 1;
+        uint8_t OS_D : 1;
+        uint8_t OS_E : 1;
+        uint8_t no_execute : 1;
     };
 } __attribute__((packed)) page_entry_t;
 
