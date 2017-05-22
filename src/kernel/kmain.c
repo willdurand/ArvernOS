a5ae16af7583aca2f595d625d74a629b782ff22d
William Durand <will+git@drnd.me>
2017-05-22 03:00:40 +0200

    Add some more output messages

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -16,26 +16,29 @@ void kmain(unsigned long magic, unsigned long addr)
     screen_clear();
 
     if (multiboot_is_valid(magic, addr) != 0) {
-        printf("System halted!");
+        printf("system halted!");
         return;
     }
 
     printf("%s\n", KERNEL_ASCII);
-    printf("%s %s / Built on: %s at %s\n", KERNEL_NAME, KERNEL_VERSION, KERNEL_DATE, KERNEL_TIME);
+    printf("%s %s / Built on: %s at %s\n\n", KERNEL_NAME, KERNEL_VERSION, KERNEL_DATE, KERNEL_TIME);
 
     isr_init();
     irq_init();
+    printf("- interruptions enabled\n");
 
     // enable scheduler
     timer_init(50);
+    printf("- clock (timer) enabled\n");
 
     // enable serial port
     serial_init(SERIAL_COM1, SERIAL_SPEED_115200);
     DEBUG("%s has started\n", KERNEL_NAME);
 
     keyboard_init();
+    printf("- keyboard routine enabled\n");
 
-    // this is needed to compute the values after
+    // TODO: this is needed to compute the values after
     dump_multiboot_info(addr);
 
     uint64_t mb_start = get_multiboot_start();
@@ -43,13 +46,16 @@ void kmain(unsigned long magic, unsigned long addr)
     uint64_t k_start = get_kernel_start();
     uint64_t k_end = get_kernel_end();
 
-    DEBUG("multiboot_start = 0x%X, multiboot_end = 0x%X", mb_start, mb_end);
-    DEBUG("kernel_start = 0x%X, kernel_end = 0x%X", k_start, k_end);
+    printf("- multiboot_start = 0x%X, multiboot_end = 0x%X\n", mb_start, mb_end);
+    printf("- kernel_start = 0x%X, kernel_end = 0x%X\n", k_start, k_end);
 
     // memory
     multiboot_tag_mmap_t *mmap = find_multiboot_tag(addr, MULTIBOOT_TAG_TYPE_MMAP);
     mmap_init(mmap, k_start, k_end, addr, (addr + *(unsigned *) addr));
     paging_init();
+    printf("- frame allocator and paging enabled\n");
+
+    printf("- system ready, welcome!\n\n$ ");
 
     while (1) {
         __asm__("hlt");
