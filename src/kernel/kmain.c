4195078881646b6b2fb9ec010b3c8ae6f0c2399c
William Durand <will+git@drnd.me>
2019-12-15 18:40:56 +0100

    add a comment to explain 'hlt'

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -99,6 +99,8 @@ void kmain(uint64_t addr) {
 
     while (1) {
         kshell_run(keyboard_get_last_scancode());
+        // This allows the CPU to enter a sleep state in which it consumes much
+        // less energy. See: https://en.wikipedia.org/wiki/HLT_(x86_instruction)
         __asm__("hlt");
     }
 }
