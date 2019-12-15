b5f73dbf2b48686303c525e0751d529edb7c28a0
William Durand <will+git@drnd.me>
2019-12-15 18:44:43 +0100

    Update keyboard docs

--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -1,4 +1,7 @@
-/** @file */
+/**
+ * @file
+ * @see https://wiki.osdev.org/PS/2_Keyboard
+ */
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 #include <stdint.h>
@@ -7,12 +10,14 @@
 #define KEYBOARD_STATUS_PORT    0x64
 
 /**
- * Initializes the keyboard driver.
+ * Initializes the keyboard (PS/2 controller) driver.
  */
 void keyboard_init();
 
 /**
  * Returns the last scancode read from the keyboard or `0`.
+ *
+ * @see https://en.wikipedia.org/wiki/Scancode
  *
  * @return the last scancode read or `0`
  */
