72da63da210945b6f5af95287a33f2bf9866026d
William Durand <will+git@drnd.me>
2019-12-18 22:33:10 +0100

    Use getchar() and printf() in init() program

--- init/init.c
+++ init/init.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("this is the init program");
+    for (int i = 0; i < 3; i++) {
+        printf("(%d) please press a key: %c\n", i, getchar());
+    }
 
     return 0;
 }
