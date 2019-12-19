84074288d0b4a36510c8fb77d732cd6e405b0353
William Durand <will+git@drnd.me>
2019-12-19 01:09:50 +0100

    [init] port cdate to the new init shell \o/

---
+++ init/cdate.h
@@ -0,0 +1,20 @@
+#ifndef CDATE_H
+#define CDATE_H
+
+#include <time.h>
+
+struct  s_cdate {
+    time_t time;
+    struct tm* tm;
+};
+
+typedef struct s_cdate t_cdate;
+
+void cdate();
+void cdate_init(t_cdate*);
+void cdate_str(t_cdate*);
+void cdate_daytag(t_cdate*);
+void cdate_calendar(t_cdate*);
+int cdate_daycount(t_cdate*);
+
+#endif
