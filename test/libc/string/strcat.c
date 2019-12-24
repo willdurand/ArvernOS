b349206254e5003d4ef947356a2c977d3064faaf
William Durand <will+git@drnd.me>
2019-12-24 22:28:35 +0100

    Add tests for strcat()

---
+++ test/libc/string/strcat.c
@@ -0,0 +1,20 @@
+#include <test.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    const char* s1 = "s1";
+    const char* s2 = "s2";
+    char* s = malloc(5 * sizeof(char));
+    strcpy(s, "");
+
+    strcat(s, s1);
+    assert(strcmp(s, "s1") == 0, "appends the string src to dest");
+    strcat(s, s2);
+    assert(strcmp(s, "s1s2") == 0, "appends the string src to dest");
+
+    free(s);
+
+    return test_summary();
+}
