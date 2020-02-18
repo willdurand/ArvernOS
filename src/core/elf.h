fae7412c1bda6f8663ef9f5091da26fab0d62abd
Nuno Silva <little.coding.fox@gmail.com>
2020-02-18 23:12:35 +0000

    Fix for paging allocs of multipages
    Started implementing better ELF64 loader

--- src/core/elf.h
+++ src/core/elf.h
@@ -4,40 +4,149 @@
 
 #include <stdint.h>
 
-#define ELF_TYPE_EXECUTABLE 0x2
+#define ELF_TYPE_NONE               0 //No file type
+#define ELF_TYPE_REL                1 //Relocatable object file
+#define ELF_TYPE_EXECUTABLE         2 //Executable file
+#define ELF_TYPE_DYN                3 //Shared object file
+#define ELF_TYPE_CORE               4 //Core file
+#define ELF_TYPE_LOOS               0xFE00 //Environment-specific use
+#define ELF_TYPE_HIOS               0xFEFF //Environment-specific use
+#define ELF_TYPE_LOPROC             0xFF00 //Processor-specific use
+#define ELF_TYPE_HIPROC             0xFFFF //Processor-specific use
 
-#define ELF_PT_LOAD 0x1
+#define ELF_PROGRAM_TYPE_NULL       0 //Unused entry
+#define ELF_PROGRAM_TYPE_LOAD       1 //Loadable segment
+#define ELF_PROGRAM_TYPE_DYNAMIC    2 //Dynamic linking tables
+#define ELF_PROGRAM_TYPE_INTERP     3 //Program interpreter path name
+#define ELF_PROGRAM_TYPE_NOTE       4 //Note sections
 
 #define ELF_PT_X 0x1
 #define ELF_PT_R 0x2
 #define ELF_PT_W 0x4
 
+#define ELF_ID_MAG0                 0 //File identification
+#define ELF_ID_MAG1                 1 //File identification
+#define ELF_ID_MAG2                 2 //File identification
+#define ELF_ID_MAG3                 3 //File identification
+#define ELF_ID_CLASS                4 //File Class
+#define ELF_ID_DATA                 5 //Data Encoding
+#define ELF_ID_VERSION              6 //File Version
+#define ELF_ID_OSABI                7 //OS/ABI Identification
+#define ELF_ID_ABIVERSION           8 //ABI Version
+#define ELF_ID_PAD                  9 //Start of padding bytes
+#define ELF_ID_NIDENT               16 //Size of identity
+
+#define ELF_CLASS32                 1 //32-bit object
+#define ELF_CLASS64                 2 //64-bit object
+
+#define ELF_DATA_2LSB               1 //Little-endian
+#define ELF_DATA_2MSB               2 //Big-endian
+
+#define ELF_SECTION_INDEX_UNDEFINED 0 //Undefined or meaningless section reference
+#define ELF_SECTION_INDEX_LOPROC    0xFF00 //Processor-specific use
+#define ELF_SECTION_INDEX_HIPROC    0xFF1F //Processor-specific use
+#define ELF_SECTION_INDEX_LOOS      0xFF20 //Environment-specific use
+#define ELF_SECTION_INDEX_HIOS      0xFF3F //Environment-specific use
+#define ELF_SECTION_INDEX_ABS       0xFFF1 //Absolute value
+#define ELF_SECTION_INDEX_COMMON    0xFFF2 //Common block
+
+#define ELF_SECTION_TYPE_NULL       0 //Unused section header
+#define ELF_SECTION_TYPE_PROGBITS   1 //Information defined by the program
+#define ELF_SECTION_TYPE_SYMTAB     2 //Linker Symbol Table
+#define ELF_SECTION_TYPE_STRTAB     3 //String Table
+#define ELF_SECTION_TYPE_RELA       4 //Rela-type relocation entries
+#define ELF_SECTION_TYPE_HASH       5 //Symbol hash table
+#define ELF_SECTION_TYPE_DYNAMIC    6 //Dynamic Linking Tables
+#define ELF_SECTION_TYPE_NOTE       7 //Note Information
+#define ELF_SECTION_TYPE_NOBITS     8 //Uninitialized Space, doesn't use any space in the file
+#define ELF_SECTION_TYPE_REL        9 //Rel-type relocation entries
+#define ELF_SECTION_TYPE_SHLIB      10 //Reserved
+#define ELF_SECTION_TYPE_DYNSYM     11 //Dynamic Loader Symbol Table
+#define ELF_SECTION_TYPE_LOOS       0x60000000 //Environment-specific use
+#define ELF_SECTION_TYPE_HOOS       0x6FFFFFFF //Environment-specific use
+#define ELF_SECTION_TYPE_LOPROC     0x70000000 //Processor-specific use
+#define ELF_SECTION_TYPE_HIPROC     0x7FFFFFFF //Processor-specific use
+
+#define ELF_SECTION_FLAG_WRITE      0x1 //Contains writable data
+#define ELF_SECTION_FLAG_ALLOC      0x2 //Section is allocated in memory image of program
+#define ELF_SECTION_FLAG_EXECINSTR  0x4 //Contains executable instructions
+#define ELF_SECTION_FLAG_MASKOS     0x0F000000 //Environment-specific use
+#define ELF_SECTION_FLAG_MASKPROC   0xF0000000 //Processor-specific use
+
+#define ELF_R_SYM(i) ((i) >> 32)
+#define ELF_R_TYPE ((i) & 0xFFFFFFFFL)
+#define ELF_R_INFO(s, t) (((s) << 32) + ((t) & 0xFFFFFFFFL))
+
 typedef struct elf_header {
-    unsigned char identity[16];
-    uint16_t type;
-    uint16_t machine;
-    uint32_t version;
-    uint64_t entry;
-    uint64_t ph_offset;
-    uint64_t sh_offset;
-    uint32_t flags;
-    uint16_t header_size;
-    uint16_t ph_size;
-    uint16_t ph_num;
-    uint16_t sh_size;
-    uint16_t sh_num;
-    uint16_t strtab_index;
+
+    unsigned char identity[16]; //ELF Identification
+    uint16_t type; //Object file type
+    uint16_t machine; //Machine type
+    uint32_t version; //Object file version
+    uint64_t entry; //Entry point address
+    uint64_t ph_offset; //Program header offset
+    uint64_t sh_offset; //Section header offset
+    uint32_t flags; //Processor-specific flags
+    uint16_t header_size; //ELF Header size
+    uint16_t ph_size; //Size of program header entry
+    uint16_t ph_num; //Number of program header entries
+    uint16_t sh_size; //Size of section header entry
+    uint16_t sh_num; //Number of section header entries
+    uint16_t strtab_index; //Section Name String Table Index
+
 } __attribute__((packed)) elf_header_t;
 
+typedef struct elf_section_header {
+
+    uint32_t name; //Section Name
+    uint32_t type; //Section Type
+    uint64_t flags; //Section attributes
+    uint64_t addr; //Virtual Address in memory
+    uint64_t offset; //Offset in file
+    uint64_t size; //Size of section
+    uint32_t link; //Link to other section
+    uint32_t info; //Miscellaneous information
+    uint64_t addralign; //Address alignment boundary
+    uint64_t entsize; //Size of entries, if section has table
+
+} __attribute__((packed)) elf_section_header_t;
+
+typedef struct elf_symbol {
+
+    uint32_t name; //Symbol name
+    uint8_t info; //Type and binding attributes
+    uint8_t other; //Reserved
+    uint16_t sectionTableIndex; //Section table index
+    uint64_t value; //Symbol value
+    uint64_t size; //Size of object
+
+} __attribute__((packed)) elf_symbol_t;
+
+typedef struct elf_rel {
+
+    uint64_t offset; //Address of reference
+    uint64_t info; //Symbol Index and type of relocation
+} __attribute__((packed)) elf_rel_t;
+
+typedef struct elf_rela {
+
+    uint64_t offset; //Address of reference
+    uint64_t info; //Symbol index and type of relocation
+    int64_t addend; //Constant part of expression
+
+} __attribute__((packed)) elf_rela_t;
+
 typedef struct elf_program_header {
-    uint32_t type;
-    uint32_t flags;
-    uint64_t offset;
-    uint64_t virtual_address;
-    uint64_t physical_address;
-    uint64_t file_size;
-    uint64_t mem_size;
-    uint64_t align;
+
+    uint32_t type; //Type of segment
+    uint32_t flags; //Segment Attributes
+    uint64_t offset; //Offset in file
+    uint64_t virtual_address; //Virtual address in memory
+    uint64_t physical_address; //Reserved
+    uint64_t file_size; //Size of segment in file
+    uint64_t mem_size; //Size of segment in memory
+    uint64_t align; //Alignment of segment
+
 } __attribute__((packed)) elf_program_header_t;
 
 elf_header_t* elf_load(uint64_t* data);
