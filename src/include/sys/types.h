55c0e9521cc58e9e5b1f38764d4deb5473e75f51
Nuno Silva <little.coding.fox@gmail.com>
2020-02-25 17:01:42 +0000

    Added stdbool.h and internal optional structs

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -2,8 +2,32 @@
 #define SYS_TYPES_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef int64_t time_t;
 typedef int64_t suseconds_t;
+
+#ifndef __is_libc
+typedef struct opt_uint8 {
+    bool valid;
+    uint8_t value;
+} opt_uint8_t;
+
+typedef struct opt_uint16 {
+    bool valid;
+    uint16_t value;
+} opt_uint16_t;
+
+typedef struct opt_uint32 {
+    bool valid;
+    uint32_t value;
+} opt_uint32_t;
+
+typedef struct opt_uint64 {
+    bool valid;
+    uint64_t value;
+} opt_uint64_t;
+
+#endif
 
 #endif
