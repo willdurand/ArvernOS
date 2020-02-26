e1c3ba292700911cd797df4565c8cee0b39d8543
Nuno Silva <little.coding.fox@gmail.com>
2020-02-26 19:31:47 +0000

    Add internal optional structs (#47)

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -2,8 +2,32 @@
 #define SYS_TYPES_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef int64_t time_t;
 typedef int64_t suseconds_t;
+
+#ifndef __is_libc
+typedef struct opt_uint8 {
+    bool has_value;
+    uint8_t value;
+} opt_uint8_t;
+
+typedef struct opt_uint16 {
+    bool has_value;
+    uint16_t value;
+} opt_uint16_t;
+
+typedef struct opt_uint32 {
+    bool has_value;
+    uint32_t value;
+} opt_uint32_t;
+
+typedef struct opt_uint64 {
+    bool has_value;
+    uint64_t value;
+} opt_uint64_t;
+
+#endif
 
 #endif
