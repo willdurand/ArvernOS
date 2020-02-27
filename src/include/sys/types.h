663917a7a99e5721d7fa0557a2d0b6215762b9c2
William Durand <will+git@drnd.me>
2020-02-27 10:55:38 +0100

    Fix fmt in src/include/sys/types.h (#51)

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -1,31 +1,35 @@
 #ifndef SYS_TYPES_H
 #define SYS_TYPES_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef int64_t time_t;
 typedef int64_t suseconds_t;
 
 #ifndef __is_libc
-typedef struct opt_uint8 {
-    bool has_value;
-    uint8_t value;
+typedef struct opt_uint8
+{
+  bool has_value;
+  uint8_t value;
 } opt_uint8_t;
 
-typedef struct opt_uint16 {
-    bool has_value;
-    uint16_t value;
+typedef struct opt_uint16
+{
+  bool has_value;
+  uint16_t value;
 } opt_uint16_t;
 
-typedef struct opt_uint32 {
-    bool has_value;
-    uint32_t value;
+typedef struct opt_uint32
+{
+  bool has_value;
+  uint32_t value;
 } opt_uint32_t;
 
-typedef struct opt_uint64 {
-    bool has_value;
-    uint64_t value;
+typedef struct opt_uint64
+{
+  bool has_value;
+  uint64_t value;
 } opt_uint64_t;
 
 #endif
