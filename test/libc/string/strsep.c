6bac0914071f93cb4a148635d9ce3e0669dddb1c
William Durand <will+git@drnd.me>
2020-03-01 21:54:45 +0100

    Fix memory leaks in some libc tests

--- test/libc/string/strsep.c
+++ test/libc/string/strsep.c
@@ -6,18 +6,22 @@
 int main()
 {
   char* string = strdup("ho hoo hoooo");
+  char* original_string = string;
   char* found;
 
   found = strsep(&string, " ");
   assert(strlen(found) == 2, "returns the first part");
+
   found = strsep(&string, " ");
   assert(strlen(found) == 3, "returns the second part");
+
   found = strsep(&string, " ");
   assert(strlen(found) == 5, "returns the last part");
+
   found = strsep(&string, " ");
   assert(found == 0, "returns 0 when there is no more part");
 
-  free(string);
+  free(original_string);
 
   return test_summary();
 }
