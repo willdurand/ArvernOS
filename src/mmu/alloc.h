8a936a35c5e6d1f3ef3be91985cd1052159428d5
William Durand <will+git@drnd.me>
2020-11-28 10:32:53 +0100

    Fix alloc docs

--- src/mmu/alloc.h
+++ src/mmu/alloc.h
@@ -7,8 +7,8 @@
 
 /// The virtual address to the start of the heap memory.
 #define HEAP_START 0x40000000
-/// The size of the heap.
-#define HEAP_SIZE (200 * 1024) // 200 KiB
+/// The size of the heap (200 KiB).
+#define HEAP_SIZE (200 * 1024)
 /// The number of pages that we can allocate.
 #define MAX_PAGES (HEAP_SIZE / PAGE_SIZE)
 
