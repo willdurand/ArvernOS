fa807d0288a049a56ea4385550f2de71e5736dd5
Nuno Silva <little.coding.fox@gmail.com>
2020-12-01 13:46:40 +0000

    Make fmt fixes

--- src/drivers/video/grub-framebuffer/grub-framebuffer.c
+++ src/drivers/video/grub-framebuffer/grub-framebuffer.c
@@ -2,65 +2,68 @@
 #include <core/multiboot.h>
 #include <drivers/video/video_console.h>
 #include <mmu/paging.h>
-#include <vtconsole/vtconsole.h>
 #include <resources/psf1/psf1.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vtconsole/vtconsole.h>
 
 #include "grub-framebuffer.h"
 
 uint32_t framebuffer_cursor_x = 0;
 uint32_t framebuffer_cursor_y = 0;
 
-#define PLOT_8_BPP_PIXEL(color, index) \
-                                {\
-                                uint8_t *pixel = (uint8_t*)framebuffer_ptr + (index);\
-                                \
-                                *pixel = (color);\
-                                }
+#define PLOT_8_BPP_PIXEL(color, index)                                                      \
+    {                                                                                       \
+        uint8_t* pixel = (uint8_t*)framebuffer_ptr + (index);                               \
+                                                                                            \
+        *pixel = (color);                                                                   \
+    }
 
-#define PLOT_16_BPP_PIXEL(color, index) \
-                                {\
-                                uint16_t *pixel = (uint16_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint16_t));\
-                                \
-                                *pixel = (color);\
-                                }
+#define PLOT_16_BPP_PIXEL(color, index)                                                     \
+    {                                                                                       \
+        uint16_t *pixel =                                                                   \
+            (uint16_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint16_t));            \
+                                                                                            \
+        *pixel = (color);                                                                   \
+    }
 
-#define PLOT_24_BPP_PIXEL(color, index) \
-                                {\
-                                uint32_t *pixel = (uint32_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint8_t[3]));\
-                                \
-                                *pixel = ((color) & 0xffffff) | (*pixel & 0xff000000);\
-                                }
+#define PLOT_24_BPP_PIXEL(color, index)                                                     \
+    {                                                                                       \
+        uint32_t *pixel =                                                                   \
+            (uint32_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint8_t[3]));          \
+                                                                                            \
+        *pixel = ((color) & 0xffffff) | (*pixel & 0xff000000);                              \
+    }
 
-#define PLOT_32_BPP_PIXEL(color, index) \
-                                {\
-                                uint32_t *pixel = (uint32_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint32_t));\
-                                \
-                                *pixel = (color);\
-                                }
+#define PLOT_32_BPP_PIXEL(color, index)                                                     \
+    {                                                                                       \
+        uint32_t *pixel =                                                                   \
+            (uint32_t*)((uint8_t*)framebuffer_ptr + (index) * sizeof(uint32_t));            \
+                                                                                            \
+        *pixel = (color);                                                                   \
+    }
 
-//Kernel Externals
+//  Kernel Externals
 
-extern PSF1_font_t *kernel_console_font;
+extern PSF1_font_t* kernel_console_font;
 extern vtconsole_t vtc;
 
-//Framebuffer Common
+//  Framebuffer Common
 
-volatile void *framebuffer_ptr = NULL;
+volatile void* framebuffer_ptr = NULL;
 uint8_t framebuffer_type = MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED;
 uint32_t framebuffer_width = 0;
 uint32_t framebuffer_height = 0;
 uint8_t framebuffer_bpp = 0;
 uint32_t framebuffer_pitch = 0;
 
-//Indexed
+//  Indexed
 
 multiboot_color_t *framebuffer_palette = NULL;
 uint16_t framebuffer_palette_num_colors = 0;
 
-//RGB
+//  RGB
 uint8_t framebuffer_red_mask_size = 0;
 uint8_t framebuffer_green_mask_size = 0;
 uint8_t framebuffer_blue_mask_size = 0;
@@ -69,33 +72,33 @@ uint8_t framebuffer_red_field_position = 0;
 uint8_t framebuffer_green_field_position = 0;
 uint8_t framebuffer_blue_field_position = 0;
 
-//Public
+//  Public
 
 uint8_t grub_framebuffer_bytes_per_pixel = 0;
 
 bool grub_framebuffer_available()
 {
-    return framebuffer_ptr != NULL && framebuffer_type != MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT;
+    return framebuffer_ptr != NULL &&
+        framebuffer_type != MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT;
 }
 
 const char *grub_framebuffer_type_string()
 {
-    if(framebuffer_ptr == NULL)
-    {
+    if(framebuffer_ptr == NULL) {
         return "(None)";
     }
 
     switch(framebuffer_type)
     {
-        case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
+    case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
 
             return "EGA Text";
 
-        case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
+    case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
 
             return "Indexed";
 
-        case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
+    case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
 
             return "RGB";
     }
@@ -103,42 +106,46 @@ const char *grub_framebuffer_type_string()
     return "(Unknown)";
 }
 
-void grub_framebuffer_clear_region(uint32_t color, uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+void grub_framebuffer_clear_region(uint32_t color,
+                                   uint32_t x,
+                                   uint32_t y,
+                                   uint32_t width,
+                                   uint32_t height)
 {
-    for(uint32_t y_pos = 0, y_index = y * framebuffer_width; y_pos < height; y_pos++, y_index += framebuffer_width)
-    {
-        for(uint32_t x_pos = 0, x_index = x + y_index; x_pos < width; x_pos++, x_index++)
-        {
-            switch(framebuffer_type)
-            {
+    for(uint32_t y_pos = 0, y_index = y * framebuffer_width; y_pos < height;
+        y_pos++, y_index += framebuffer_width) {
+        for(uint32_t x_pos = 0, x_index = x + y_index; x_pos < width; 
+            x_pos++, x_index++) {
+            switch(framebuffer_type) {
                 case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
 
-                switch(framebuffer_bpp)
-                {
-                    case 8:
+                    switch(framebuffer_bpp)
+                    {
+                        case 8:
 
-                        PLOT_8_BPP_PIXEL(color, x_index);
+                            PLOT_8_BPP_PIXEL(color, x_index);
 
-                        break;
+                            break;
 
-                    case 15:
-                    case 16:
+                        case 15:
+                        case 16:
 
-                        PLOT_16_BPP_PIXEL(color, x_index);
+                            PLOT_16_BPP_PIXEL(color, x_index);
 
-                        break;
+                            break;
 
-                    case 24:
+                        case 24:
 
-                        PLOT_24_BPP_PIXEL(color, x_index);
+                            PLOT_24_BPP_PIXEL(color, x_index);
 
-                        break;
+                            break;
 
-                    case 32:
+                        case 32:
 
-                        PLOT_32_BPP_PIXEL(color, x_index);
+                            PLOT_32_BPP_PIXEL(color, x_index);
 
-                        break;
+                            break;
+                    }
 
                 case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
 
@@ -151,7 +158,6 @@ void grub_framebuffer_clear_region(uint32_t color, uint32_t x, uint32_t y, uint3
                     //Unused
 
                     break;
-                }
             }
         }
     }
