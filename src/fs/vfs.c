58b53360567a24d50b8a48e6ba6a28f23b28bea4
William Durand <will+git@drnd.me>
2020-12-18 20:18:21 +0100

    Refactor VFS

--- src/fs/vfs.c
+++ src/fs/vfs.c
@@ -1,18 +1,22 @@
 #include "vfs.h"
 #include "logging.h"
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 inode_t vfs_find_root(char** path);
 inode_t vfs_namei_mount(const char* path, inode_t root);
 
-static vfs_node_t* vfs_root;
+static vfs_node_t* vfs_root = NULL;
 
 void vfs_init()
 {
   vfs_root = calloc(1, sizeof(vfs_node_t));
-  vfs_root->parent = vfs_root;
   strcpy(vfs_root->name, "/");
+  vfs_root->parent = vfs_root;
+  vfs_root->type = FS_DIRECTORY | FS_MOUNTPOINT;
+  vfs_root->n_children = 0;
+  vfs_root->children = (inode_t*)calloc(0, sizeof(inode_t));
 }
 
 uint64_t vfs_open(inode_t inode, uint64_t mode)
@@ -71,25 +75,46 @@ uint64_t vfs_isatty(inode_t inode)
 
 dirent_t* vfs_readdir(inode_t inode, uint64_t num)
 {
-  // Support '.' and '..' special files in directories.
-  if (vfs_inode_type(inode) == FS_DIRECTORY) {
-    if (num == 0) {
-      dirent_t* ret = calloc(1, sizeof(dirent_t));
-      ret->inode = inode;
-      strcpy(ret->name, ".");
-      return ret;
-    }
-
-    if (num == 1) {
-      dirent_t* ret = calloc(1, sizeof(dirent_t));
-      ret->inode = inode->parent;
-      strcpy(ret->name, "..");
-      return ret;
-    }
+  if (inode == NULL) {
+    FS_DEBUG("%s", "inode is NULL");
+    return NULL;
   }
 
-  dirent_t* de = 0;
+  if (vfs_inode_type(inode) != FS_DIRECTORY) {
+    FS_DEBUG("%s", "inode is not a directory");
+    return NULL;
+  }
+
+  dirent_t* de = NULL;
+
+  if (num == 0) {
+    de = calloc(1, sizeof(dirent_t));
+    de->inode = inode;
+    strcpy(de->name, ".");
+    return de;
+  }
+
+  if (num == 1) {
+    de = calloc(1, sizeof(dirent_t));
+    de->inode = inode->parent;
+    strcpy(de->name, "..");
+    return de;
+  }
+
+  bool is_mountpoint = (inode->type & FS_MOUNTPOINT) == FS_MOUNTPOINT;
+
+  if (is_mountpoint && inode->n_children > (num - 2)) {
+    de = calloc(1, sizeof(dirent_t));
+    de->inode = inode->children[num - 2];
+    strcpy(de->name, inode->children[num - 2]->name);
+    return de;
+  }
+
   if (inode->driver && inode->driver->readdir) {
+    if (is_mountpoint) {
+      num -= inode->n_children;
+    }
+
     de = inode->driver->readdir(inode, num);
   }
 
@@ -99,29 +124,20 @@ dirent_t* vfs_readdir(inode_t inode, uint64_t num)
 inode_t vfs_finddir(inode_t inode, const char* name)
 {
   if (vfs_inode_type(inode) == FS_DIRECTORY) {
-    if (!strncmp(name, ".", strlen(name))) {
+    if (!strcmp(name, ".")) {
       return inode;
     }
 
-    if (!strncmp(name, "..", strlen(name))) {
+    if (!strcmp(name, "..")) {
       return inode->parent;
     }
-  }
 
-  // Try to return mounted filesystems first.
-  inode_t ino = inode->child;
-  while (ino) {
-    if (strncmp(name, ino->name, strlen(name)) == 0) {
-      return ino;
+    if (inode->driver && inode->driver->finddir) {
+      return inode->driver->finddir(inode, name);
     }
-    ino = ino->older;
   }
 
-  if (inode->driver && inode->driver->finddir) {
-    return inode->driver->finddir(inode, name);
-  }
-
-  return 0;
+  return NULL;
 }
 
 inode_t vfs_find_root(char** path)
@@ -130,21 +146,16 @@ inode_t vfs_find_root(char** path)
   inode_t mount = current;
 
   char* name = NULL;
-
   while ((name = strsep(path, "/")) != 0) {
-    FS_DEBUG("current->name=%s name=%s", current->name, name);
-    current = current->child;
-
-    while (current) {
-      if (strcmp(current->name, name) == 0) {
-        mount = current;
+    uint64_t i = 0;
+    for (; i < current->n_children; i++) {
+      if (strcmp(current->children[i]->name, name) == 0) {
+        mount = current->children[i];
         break;
       }
-
-      current = current->older;
     }
 
-    if (!current) {
+    if (i == current->n_children) {
       if (*path) {
         *path = *path - 1;
         *path[0] = '/';
@@ -155,7 +166,7 @@ inode_t vfs_find_root(char** path)
     }
   }
 
-  FS_DEBUG("found root=%s", mount->name);
+  FS_DEBUG("found root node: %s", mount->name);
 
   return mount;
 }
@@ -169,97 +180,179 @@ inode_t vfs_namei_mount(const char* path, inode_t root)
 
   if (strcmp(path, "/") != 0) {
     char* name = NULL;
-
     while (current && (name = strsep(&pth, "/")) != 0) {
+      FS_DEBUG("current node is: %s", current->name);
+
       inode_t next = vfs_finddir(current, name);
 
-      if (!next) {
-        FS_DEBUG("%s", "no next, returning 0");
-        vfs_free(current);
-        free(npath);
-        return 0;
+      if (next == NULL) {
+        for (uint64_t i = 0; i < current->n_children; i++) {
+          if (strcmp(current->children[i]->name, name) == 0) {
+            next = current->children[i];
+            break;
+          }
+        }
       }
-
-      FS_DEBUG("name=%s next_name=%s", name, next->name);
 
       if (root) {
+        if (!next) {
+          if (pth != NULL) {
+            FS_DEBUG("cannot create a node because pth=%s", pth);
+            free(npath);
+            return NULL;
+          }
+
+          next = calloc(1, sizeof(vfs_node_t));
+          next->n_children = 0;
+          next->children = (inode_t*)calloc(0, sizeof(inode_t));
+          next->type = FS_DIRECTORY;
+          strcpy(next->name, name);
+
+          FS_DEBUG("created node: %s", next->name);
+        }
+
         next->parent = current;
-        next->older = current->child;
-        current->child = next;
+
+        FS_DEBUG("adding child #%lu to current node: %s",
+                 current->n_children + 1,
+                 current->name);
+        current->children = (inode_t*)realloc(
+          current->children, (current->n_children + 1) * sizeof(inode_t));
+        current->children[current->n_children++] = next;
       }
 
-      vfs_free(current);
+      if (next == NULL) {
+        FS_DEBUG("%s", "next is NULL, returning NULL");
+        free(npath);
+        return NULL;
+      }
+
       current = next;
     }
   }
 
   free(npath);
 
-  if (root != 0) {
-    root->parent = current->parent;
-    current->parent = 0;
+  if (root != NULL && vfs_inode_type(current) == FS_DIRECTORY) {
+    FS_DEBUG("attempting to mount root_fs=%s at node=%s (parent=%s)",
+             root->name,
+             current->name,
+             current->parent->name);
 
-    if (root->parent->child == current) {
-      root->parent->child = root;
-    }
+    current->type = root->type | FS_MOUNTPOINT;
+    current->driver = root->driver;
+    current->data = root->data;
 
-    root->older = current->older;
-
-    if (root->older) {
-      root->older->younger = current;
-    }
-
-    root->younger = current->younger;
-
-    if (root->younger) {
-      root->younger->older = current;
-    }
-
-    strcpy(root->name, current->name);
-    root->type |= FS_MOUNTPOINT;
-
-    if (current == vfs_root) {
-      vfs_root = root;
-    }
-
-    vfs_free(current);
-    current = root;
+    vfs_free(root);
   }
 
-  FS_DEBUG("returning current=%s", current->name);
+  FS_DEBUG("returning current node: %s", current->name);
 
   return current;
 }
 
 inode_t vfs_mount(const char* path, inode_t root)
 {
-  FS_DEBUG("mounting path=%s root_fs=%s", path, root->name);
+  if (vfs_root == NULL) {
+    DEBUG("%s", "vfs_root is NULL. vfs_init() not called?");
+    return NULL;
+  }
+
+  if (path == NULL || path[0] != '/') {
+    DEBUG("path must be absolute: %s", path);
+    return NULL;
+  }
+
+  FS_DEBUG("trying to mount fs=%s at path=%s", root->name, path);
   return vfs_namei_mount(path, root);
 }
 
 inode_t vfs_namei(const char* path)
 {
-  FS_DEBUG("finding path=%s", path);
-  return vfs_namei_mount(path, 0);
+  FS_DEBUG("trying to find node for path=%s", path);
+  return vfs_namei_mount(path, NULL);
 }
 
-inode_t vfs_umount(const char* path)
+int vfs_umount(const char* path)
 {
-  FS_DEBUG("umounting path=%s", path);
-  // TODO: implement me
+  FS_DEBUG("trying to unmount path=%s", path);
+  inode_t node = vfs_namei(path);
+
+  if (node == NULL) {
+    FS_DEBUG("no node found for path=%s", path);
+    return FS_ERR_NOTFOUND;
+  }
+
+  if ((node->type & FS_MOUNTPOINT) != FS_MOUNTPOINT) {
+    FS_DEBUG("%s", "node is not mountpoint");
+    return FS_ERR_NOTAMOUNTPOINT;
+  }
+
+  if (node->n_children > 0) {
+    FS_DEBUG("%s", "node has children");
+    return FS_ERR_HASCHILDREN;
+  }
+
+  inode_t parent = node->parent;
+  inode_t* new_children =
+    (inode_t*)malloc(sizeof(inode_t) * parent->n_children - 1);
+
+  for (uint64_t i = 0, j = 0; i < parent->n_children; i++) {
+    if (parent->children[i] != node) {
+      new_children[j++] = parent->children[i];
+    }
+  }
+
+  free(parent->children);
+  parent->children = new_children;
+  parent->n_children--;
+
+  node->parent = NULL;
+  vfs_free(node);
+  node = NULL;
+
   return 0;
 }
 
-void vfs_free(inode_t inode)
+int vfs_free(inode_t inode)
 {
-  if (inode->parent != 0) {
-    return;
+  if (inode->parent != NULL) {
+    DEBUG("%s", "cannot free a node with a parent");
+    return FS_ERR_HASPARENT;
   }
 
+  FS_DEBUG("free'ing node: %s", inode->name);
+
+  for (uint64_t i = 0; i < inode->n_children; i++) {
+    inode->children[i]->parent = NULL;
+    vfs_free(inode->children[i]);
+  }
+
+  free(inode->children);
   free(inode);
+
+  return 0;
 }
 
 int vfs_inode_type(inode_t inode)
 {
   return inode->type & FS_MASK;
+}
+
+void vfs_debug(inode_t node, int depth)
+{
+  if (depth == 0) {
+    printf(".\n");
+  }
+
+  for (int i = 0; i < (depth * 4); i++) {
+    printf(" ");
+  }
+  printf("└── %s\n", node->name);
+
+  depth++;
+
+  for (int i = 0; i < node->n_children; i++) {
+    vfs_debug(node->children[i], depth);
+  }
 }
--- src/fs/vfs.h
+++ src/fs/vfs.h
@@ -14,6 +14,11 @@
 #define FS_SYMLINK     0x40
 #define FS_MASK        0xFE
 
+#define FS_ERR_NOTAMOUNTPOINT 1
+#define FS_ERR_HASCHILDREN    2
+#define FS_ERR_NOTFOUND       3
+#define FS_ERR_HASPARENT      4
+
 struct vfs_node;
 
 typedef struct vfs_node* inode_t;
@@ -44,28 +49,42 @@ typedef struct vfs_driver
 typedef struct vfs_node
 {
   char name[VFS_NAME_MAX_SIZE];
-  inode_t parent;
-  inode_t child;
-  inode_t older;
-  inode_t younger;
   uint64_t type;
   vfs_driver_t* driver;
+  inode_t parent;
+  uint64_t n_children;
+  inode_t* children;
   int data;
 } vfs_node_t;
 
 void vfs_init();
+
 uint64_t vfs_open(inode_t inode, uint64_t mode);
+
 uint64_t vfs_close(inode_t inode);
+
 uint64_t vfs_read(inode_t inode, void* ptr, uint64_t length, uint64_t offset);
+
 uint64_t vfs_write(inode_t inode, void* ptr, uint64_t length, uint64_t offset);
+
 uint64_t vfs_stat(inode_t inode, stat_t* stat);
+
 uint64_t vfs_isatty(inode_t inode);
+
 dirent_t* vfs_readdir(inode_t inode, uint64_t num);
+
 inode_t vfs_finddir(inode_t inode, const char* name);
+
 inode_t vfs_mount(const char* path, inode_t root);
+
 inode_t vfs_namei(const char* path);
-inode_t vfs_umount(const char* path);
-void vfs_free(inode_t inode);
+
+int vfs_umount(const char* path);
+
+int vfs_free(inode_t inode);
+
 int vfs_inode_type(inode_t inode);
+
+void vfs_debug(inode_t node, int depth);
 
 #endif
