7a4914c1a6b1671c63d8ecb49be6996f29db0be0
William Durand <will+git@drnd.me>
2020-12-18 20:16:53 +0100

    Improve logging, especially for tests

--- src/logging.h
+++ src/logging.h
@@ -3,41 +3,42 @@
 
 #include <stdio.h>
 
-#ifdef ENABLE_LOGS_FOR_TESTS
-
-#define LOG(level, level_color, format, ...)                                   \
-  printf("%-8s | %s:%d:%s(): " format "\n",                                    \
-         level,                                                                \
-         __FILE__,                                                             \
-         __LINE__,                                                             \
-         __func__,                                                             \
-         __VA_ARGS__)
-
-#else // ENABLE_LOGS_FOR_TEST
-
-#include <drivers/serial.h>
-
-static const uint16_t serial_com1 = SERIAL_COM1;
-
-#ifdef LOGS_WITH_COLORS
 // See: https://github.com/shiena/ansicolor
 #define ANSICOLOR_RESET        "\x1b[0m"
 #define ANSICOLOR_FG_CYAN      "\x1b[36m"
 #define ANSICOLOR_FG_LIGHTGRAY "\x1b[90m"
 #define ANSICOLOR_FG_YELLOW    "\x1b[33m"
 
+#ifdef ENABLE_LOGS_FOR_TESTS
+
+#define LOG(level, level_color, format, ...)                                   \
+  printf("%s%s:%lu:%s(): " format "%s\n",                                      \
+         ANSICOLOR_FG_LIGHTGRAY,                                               \
+         __FILE__,                                                             \
+         (uint64_t)__LINE__,                                                   \
+         __func__,                                                             \
+         __VA_ARGS__,                                                          \
+         ANSICOLOR_RESET)
+
+#else // ENABLE_LOGS_FOR_TESTS
+
+#include <drivers/serial.h>
+
+static const uint16_t serial_com1 = SERIAL_COM1;
+
+#ifdef LOGS_WITH_COLORS
 // We use colors because the serial port is bound to a file in debug mode (see
 // qemu config in `Makefile`). That would not work otherwise, I think.
 #define LOG(level, level_color, format, ...)                                   \
   fctprintf(&serial_stream_output,                                             \
             (void*)&serial_com1,                                               \
-            "%s%-8s%s | %s%s:%ld:%s():%s " format "\n",                        \
+            "%s%-8s%s | %s%s:%lu:%s():%s " format "\n",                        \
             level_color,                                                       \
             level,                                                             \
             ANSICOLOR_RESET,                                                   \
             ANSICOLOR_FG_LIGHTGRAY,                                            \
             __FILE__,                                                          \
-            __LINE__,                                                          \
+            (uint64_t)__LINE__,                                                \
             __func__,                                                          \
             ANSICOLOR_RESET,                                                   \
             __VA_ARGS__)
@@ -47,18 +48,18 @@ static const uint16_t serial_com1 = SERIAL_COM1;
 #define LOG(level, level_color, format, ...)                                   \
   fctprintf(&serial_stream_output,                                             \
             (void*)&serial_com1,                                               \
-            "%-8s | %s:%ld:%s(): " format "\n",                                \
+            "%-8s | %s:%lu:%s(): " format "\n",                                \
             level,                                                             \
             __FILE__,                                                          \
-            __LINE__,                                                          \
+            (uint64_t)__LINE__,                                                \
             __func__,                                                          \
             __VA_ARGS__)
 
 #endif // LOGS_WITH_COLORS
 
-#endif // ENABLE_LOGS_FOR_TEST
+#endif // ENABLE_LOGS_FOR_TESTS
 
-#ifdef ENABLE_KERNEL_DEBUG
+#if defined(ENABLE_KERNEL_DEBUG) || defined(ENABLE_LOGS_FOR_TESTS)
 
 #define DEBUG(format, ...) LOG("DEBUG", ANSICOLOR_FG_CYAN, format, __VA_ARGS__)
 
@@ -73,13 +74,13 @@ static const uint16_t serial_com1 = SERIAL_COM1;
   }                                                                            \
   fctprintf(&serial_stream_output, (void*)&serial_com1, "\n");
 
-#else // ENABLE_KERNEL_DEBUG
+#else // ENABLE_KERNEL_DEBUG || ENABLE_LOGS_FOR_TESTS
 
 #define DEBUG(format, ...)
 
 #define HEX_DEBUG(data, len)
 
-#endif // ENABLE_KERNEL_DEBUG
+#endif // ENABLE_KERNEL_DEBUG || ENABLE_LOGS_FOR_TESTS
 
 #define INFO(format, ...) LOG("INFO", ANSICOLOR_FG_YELLOW, format, __VA_ARGS__)
 
