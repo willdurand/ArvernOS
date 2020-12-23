71ae84a731b4596b4f3db55efb4fe024fae0fd70
William Durand <will+git@drnd.me>
2020-12-23 10:26:37 +0100

    Refactor kernel console to support VGA and VBE modes

--- src/kernel/console.h
+++ src/kernel/console.h
@@ -2,12 +2,17 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#include <core/multiboot.h>
 #include <stddef.h>
+#include <vtconsole/vtconsole.h>
+
+#define CONSOLE_WIDTH  80
+#define CONSOLE_HEIGHT 25
 
 /**
  * Initializes the console (VT100).
  */
-void console_init();
+void console_init(multiboot_tag_framebuffer_common_t* common);
 
 /**
  * Prints a character to the console.
@@ -24,5 +29,15 @@ void console_putchar(char c);
  * @return the number of characters printed to the console
  */
 size_t console_write(const char* s, size_t count);
+
+// Functions for the "VGA text mode"-based console.
+void vga_console_init();
+void vga_on_paint_callback(vtconsole_t* vtc, vtcell_t* cell, int x, int y);
+void vga_on_move_callback(vtconsole_t* vtc, vtcursor_t* cur);
+
+// Functions for the VBE-based console.
+void vbe_console_init(multiboot_tag_framebuffer_common_t* common);
+void vbe_on_paint_callback(vtconsole_t* vtc, vtcell_t* cell, int x, int y);
+void vbe_on_move_callback(vtconsole_t* vtc, vtcursor_t* cur);
 
 #endif
