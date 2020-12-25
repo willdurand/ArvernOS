f1e3261c596a74785acb06bd3ef3e630b8e81cb1
Nuno Silva <little.coding.fox@gmail.com>
2020-12-25 12:22:09 +0000

    libc: add initial implementation of the `abort()` function

---
+++ src/libc/abort.c
@@ -0,0 +1,16 @@
+#ifndef __is_libk
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((__noreturn__)) void abort()
+{
+  // TODO: abnormally terminate the process as if by SIGABRT.
+  printf("abort() was called\n");
+
+  while (1) {
+    ;
+  }
+  __builtin_unreachable();
+}
+
+#endif
