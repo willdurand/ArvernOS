59af8a761785bf3b2f6af052b58c074f81df0615
William Durand <will+git@drnd.me>
2020-12-27 22:41:48 +0100

    Fix IDT struct and add debug statements

--- src/core/idt.h
+++ src/core/idt.h
@@ -8,8 +8,8 @@
 
 #include <stdint.h>
 
-#define INTERRUPT_GATE 0x01
-#define TRAP_GATE      0x02
+#define INTERRUPT_GATE 0xE
+#define TRAP_GATE      0xF
 
 /**
  * The number of entries in the _Interrupt Descriptor Table_.
@@ -19,17 +19,6 @@
  */
 #define IDT_ENTRIES 256
 
-typedef struct idt_gate_options
-{
-  uint8_t index : 3;
-  uint8_t reserved : 5;
-  uint8_t type : 1;
-  uint8_t ones : 3;
-  uint8_t zero : 1;
-  uint8_t DPL : 2;
-  uint8_t present : 1;
-} __attribute__((packed)) idt_gate_options_t;
-
 /**
  * This structure represents an IDT gate.
  */
@@ -37,7 +26,18 @@ typedef struct idt_gate
 {
   uint16_t ptr_low;
   uint16_t selector;
-  idt_gate_options_t options;
+  uint8_t ist;
+  union
+  {
+    uint8_t flags;
+    struct
+    {
+      uint8_t type : 4;
+      uint8_t s : 1;
+      uint8_t dpl : 2;
+      uint8_t present : 1;
+    }
+  };
   uint16_t ptr_mid;
   uint32_t ptr_high;
   uint32_t reserved;
