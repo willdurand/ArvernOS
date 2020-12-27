44f294f4ae29571e623903af73caf8e9db12d7f1
William Durand <will+git@drnd.me>
2020-12-27 22:43:33 +0100

    Fix IDT struct and add debug statements (#189)

--- src/core/idt.c
+++ src/core/idt.c
@@ -1,4 +1,5 @@
 #include "idt.h"
+#include "logging.h"
 #include <core/gdt.h>
 
 static idt_gate_t idt[IDT_ENTRIES] = { 0 };
@@ -11,18 +12,18 @@ void idt_init()
 
 void idt_register_gate(uint16_t n, uint64_t handler, uint8_t type)
 {
-  idt[n].selector = KERNEL_BASE_SELECTOR;
   idt[n].ptr_low = (uint16_t)handler;
   idt[n].ptr_mid = (uint16_t)(handler >> 16);
   idt[n].ptr_high = (uint32_t)(handler >> 32);
-  idt[n].reserved = 0;
+  idt[n].selector = KERNEL_BASE_SELECTOR;
+  idt[n].ist = 0;
+  idt[n].type = type;
+  idt[n].s = 0;
+  idt[n].dpl = 0; // privilege to call the handler ("ring")
+  idt[n].present = 1;
 
-  idt[n].options.index = 0; // do not switch stack
-  idt[n].options.type = type;
-  idt[n].options.ones = 0x07;
-  idt[n].options.zero = 0;
-  idt[n].options.DPL = 0; // privilege to call the handler ("ring")
-  idt[n].options.present = 1;
+  CORE_DEBUG(
+    "registered gate #%03d: handler=%p flags=0x%02x", n, handler, idt[n].flags);
 }
 
 void idt_register_interrupt(uint16_t n, uint64_t handler)
