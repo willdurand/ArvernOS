9bfa72a20205653331f66edf0bf5cf1393611f62
William Durand <will+git@drnd.me>
2020-12-29 10:12:04 +0100

    s/gdt_entry/gdt_descriptor

--- src/core/gdt.h
+++ src/core/gdt.h
@@ -3,7 +3,7 @@
 
 #define KERNEL_BASE_SELECTOR 0x08
 
-typedef struct gdt_entry
+typedef struct gdt_descriptor
 {
   uint16_t limit15_0;
   uint16_t base15_0;
@@ -11,15 +11,15 @@ typedef struct gdt_entry
   uint8_t type;
   uint8_t limit19_16_and_flags;
   uint8_t base31_24;
-} gdt_entry_t;
+} gdt_descriptor_t;
 
 typedef struct gdt_table
 {
-  gdt_entry_t null_1;
-  gdt_entry_t kernel_code;
-  gdt_entry_t kernel_data;
-  gdt_entry_t tss_low;
-  gdt_entry_t tss_high;
+  gdt_descriptor_t null_1;
+  gdt_descriptor_t kernel_code;
+  gdt_descriptor_t kernel_data;
+  gdt_descriptor_t tss_low;
+  gdt_descriptor_t tss_high;
 } gdt_table_t;
 
 #endif
