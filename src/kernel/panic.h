c435a8aa2e6c4ea2edcf72feff9623df78aed922
Nuno Silva <little.coding.fox@gmail.com>
2020-12-31 22:03:00 +0000

    Added kernel stacktrace

--- src/kernel/panic.h
+++ src/kernel/panic.h
@@ -14,11 +14,19 @@
 /// This macro should only be used when something goes terribly wrong.
 #define PANIC(...) __PANIC(__VA_ARGS__, "\n")
 
+typedef struct stack_frame
+{
+  struct stack_frame* rbp;
+  uint64_t rip;
+} stack_frame_t;
+
 /**
  * Handles a kernel panic.
  *
  * @param format the reason for the kernel panic
  */
 void kernel_panic(const char* format, ...);
+
+void kernel_dump_stacktrace();
 
 #endif
