0a01a2d722ff14194fa0d9e469bc3b10f7192792
William Durand <will+git@drnd.me>
2020-12-31 22:47:07 +0100

    Do not mess with rsp in k_syscall.asm

--- src/asm/k_syscall.asm
+++ src/asm/k_syscall.asm
@@ -1,7 +1,6 @@
 global syscall_handler
 
 syscall_handler:
-  push rsp     ; save user stack
   push r11     ; save the flags
   push rcx     ; save the return address
   mov rcx, r10 ; fix 3rd syscall arg
@@ -14,5 +13,4 @@ syscall_handler:
   cli
   pop rcx
   pop r11
-  pop rsp ; restore user stack
   o64 sysret
