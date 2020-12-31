5c0b214a0ea448e0893c4d305eadc37486145b65
William Durand <will+git@drnd.me>
2020-12-31 22:22:58 +0100

    Fix syscall handler code

--- src/asm/k_syscall.asm
+++ src/asm/k_syscall.asm
@@ -1,12 +1,18 @@
 global syscall_handler
 
 syscall_handler:
-  push rcx
-  mov rcx, r10
+  push rsp     ; save user stack
+  push r11     ; save the flags
+  push rcx     ; save the return address
+  mov rcx, r10 ; fix 3rd syscall arg
   sti
+
   extern syscall_handlers
   mov r10, [rax * 8 + syscall_handlers]
   call r10
+
   cli
   pop rcx
+  pop r11
+  pop rsp ; restore user stack
   o64 sysret
