ec35df76289cf47e2228f37953ba9b87a6a86b3b
William Durand <will+git@drnd.me>
2021-01-01 01:33:55 +0100

    Use parg in init

--- userland/init/main.c
+++ userland/init/main.c
@@ -1,27 +1,29 @@
+#include <parg.h>
 #include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-  printf("init: %s started", argv[0]);
+  printf("init: %s started with pid=%d\n", argv[0], getpid());
 
-  if (argc == 1) {
-    printf(" without any argument\n");
-  } else {
-    printf(" with the following arguments:\n");
+  struct parg_state ps;
+  parg_init(&ps);
 
-    for (int i = 1; i < argc; i++) {
-      printf("  %02d: %s\n", i, argv[i]);
+  int c;
+  while ((c = parg_getopt(&ps, argc, argv, "h")) != -1) {
+    switch (c) {
+      case 'h':
+        printf("USAGE:\n  init [-h]\n");
+        return 0;
     }
   }
-
-  printf("init: pid=%d\n", getpid());
 
   const char* path = "/bin/shell";
   char* args[] = { (char*)path, NULL };
 
   printf("init: executing %s\n", path);
-  execv(path, args);
+
+  return execv(path, args);
 
   return 0;
 }
