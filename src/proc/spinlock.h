22a15aa16791ba34f96c0b1d5eb46713f07f42e9
William Durand <will+git@drnd.me>
2021-01-21 22:20:43 +0100

    Add simple spinlock (#255)

---
+++ src/proc/spinlock.h
@@ -0,0 +1,16 @@
+#ifndef PROC_SPINLOCK_H
+#define PROC_SPINLOCK_H
+
+#define MAKE_LOCK(name) volatile int name##_lock
+
+#define LOCK(name)                                                             \
+  while (!__sync_bool_compare_and_swap(&name##_lock, 0, 1)) {                  \
+    ;                                                                          \
+  }                                                                            \
+  __sync_synchronize();
+
+#define UNLOCK(name)                                                           \
+  __sync_synchronize();                                                        \
+  name##_lock = 0;
+
+#endif
