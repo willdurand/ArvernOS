5247a13feb3db1fb61fe3c70e12392dd7b5f64c2
Umar <j_umar@outlook.com>
2021-05-22 09:42:10 +0000

    implement  strpbrk

---
+++ src/libc/string/strpbrk.c
@@ -0,0 +1,21 @@
+#include <string.h>  
+/**
+ * strpbrk - searches for one of the characters in the list you give it as a string
+ * @src  :  main string  
+ * @s    :  characters list 
+ */ 
+
+extern char *
+strpbrk ( const char * src , const char *s ) {
+    char *_b  =  (char *) s ; 
+    while ( *src != *s && *src ) 
+    {
+        s++ ; 
+        if (!*s)
+        {
+            s=_b ; 
+            src++;  
+        }
+    }
+    return  (char *) src ; 
+} 
