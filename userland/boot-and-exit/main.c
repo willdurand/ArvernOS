b57f4f84f9e1d9d065d00d733d753a4aa65b839f
William Durand <will+git@drnd.me>
2021-11-10 10:17:11 +0100

    userland: boot-and-exit: write to /dev/null (smoke test)

--- userland/boot-and-exit/main.c
+++ userland/boot-and-exit/main.c
@@ -1,14 +1,25 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/reboot.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
 int main()
 {
-  printf("Looks like everything is alright!\n");
+  // Invoke some syscalls.
+  int fd = open("/dev/null", O_WRONLY);
+  int bytes_written = write(fd, NULL, 50);
+  close(fd);
+
+  if (bytes_written == 50) {
+    printf("Looks like everything is alright!\n");
 
 #ifdef __willos__
-  reboot(REBOOT_CMD_POWER_OFF);
+    reboot(REBOOT_CMD_POWER_OFF);
 #endif
 
-  return 0;
+    return 0;
+  }
+
+  return 1;
 }
