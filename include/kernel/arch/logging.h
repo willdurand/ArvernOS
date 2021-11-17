3d4af6163ee23768f342a9a87773805c710c11c3
William Durand <will+git@drnd.me>
2021-11-17 22:10:19 +0100

    kernel: add some docs to the kernel/arch/ headers (#352)

--- include/kernel/arch/logging.h
+++ include/kernel/arch/logging.h
@@ -1,9 +1,14 @@
+/**
+ * @file
+ *
+ * Architecture-specific implementation to output logs.
+ */
 #ifndef ARCH_LOGGING_H
 #define ARCH_LOGGING_H
 
 /**
- * Architecture-specific code should implement this function to output logs
- * somewhere. In most cases, a serial line will be used.
+ * This function is used to output logs somewhere. In most cases, a serial line
+ * will be used but it is up to the implementers, depending on the constraints.
  *
  * @param c a character to print
  * @param arg optional arguments. This should be `NULL` all the time.
