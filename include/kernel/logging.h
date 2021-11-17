bb8490d83af67cc8299bbb60c6753d0d01d5d319
William Durand <will+git@drnd.me>
2021-11-17 22:36:52 +0100

    kernel: logging: remove built-in ansi colors (#353)

--- include/kernel/logging.h
+++ include/kernel/logging.h
@@ -4,16 +4,13 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-// See: https://github.com/shiena/ansicolor
-#define ANSICOLOR_RESET        "\x1b[0m"
-#define ANSICOLOR_FG_CYAN      "\x1b[36m"
-#define ANSICOLOR_FG_LIGHTGRAY "\x1b[90m"
-#define ANSICOLOR_FG_YELLOW    "\x1b[33m"
-#define ANSICOLOR_FG_RED       "\x1b[31m"
-
 #ifdef ENABLE_LOGS_FOR_TESTS
 
-#define LOG(level, level_color, format, ...)                                   \
+// See: https://github.com/shiena/ansicolor
+#define ANSICOLOR_FG_LIGHTGRAY "\x1b[90m"
+#define ANSICOLOR_RESET        "\x1b[0m"
+
+#define LOG(level, format, ...)                                                \
   printf("%s%s:%" PRIu64 ":%s(): " format "%s\n",                              \
          ANSICOLOR_FG_LIGHTGRAY,                                               \
          __FILE__,                                                             \
@@ -26,26 +23,7 @@
 
 #include <arch/logging.h>
 
-#ifdef LOGS_WITH_COLORS
-// We use colors because the serial port is bound to a file in debug mode (see
-// qemu config in `Makefile`). That would not work otherwise, I think.
-#define LOG(level, level_color, format, ...)                                   \
-  fctprintf(&arch_logging_stream_output,                                       \
-            NULL,                                                              \
-            "%s%-8s%s | %s%s:%" PRIu64 ":%s():%s " format "\n",                \
-            level_color,                                                       \
-            level,                                                             \
-            ANSICOLOR_RESET,                                                   \
-            ANSICOLOR_FG_LIGHTGRAY,                                            \
-            __FILE__,                                                          \
-            (uint64_t)__LINE__,                                                \
-            __func__,                                                          \
-            ANSICOLOR_RESET,                                                   \
-            __VA_ARGS__)
-
-#else // LOGS_WITH_COLORS
-
-#define LOG(level, level_color, format, ...)                                   \
+#define LOG(level, format, ...)                                                \
   fctprintf(&arch_logging_stream_output,                                       \
             NULL,                                                              \
             "%-8s | %s:%" PRIu64 ":%s(): " format "\n",                        \
@@ -55,13 +33,11 @@
             __func__,                                                          \
             __VA_ARGS__)
 
-#endif // LOGS_WITH_COLORS
-
 #endif // ENABLE_LOGS_FOR_TESTS
 
 #if defined(ENABLE_KERNEL_DEBUG) || defined(ENABLE_LOGS_FOR_TESTS)
 
-#define DEBUG(format, ...) LOG("DEBUG", ANSICOLOR_FG_CYAN, format, __VA_ARGS__)
+#define DEBUG(format, ...) LOG("DEBUG", format, __VA_ARGS__)
 
 #define HEX_DEBUG(data, len)                                                   \
   DEBUG("(hexdump) data=%p len=%" PRIu64, data, (uint64_t)len);                \
@@ -86,8 +62,8 @@
 
 #endif // ENABLE_KERNEL_DEBUG || ENABLE_LOGS_FOR_TESTS
 
-#define INFO(format, ...) LOG("INFO", ANSICOLOR_FG_YELLOW, format, __VA_ARGS__)
+#define INFO(format, ...) LOG("INFO", format, __VA_ARGS__)
 
-#define ERROR(format, ...) LOG("ERROR", ANSICOLOR_FG_RED, format, __VA_ARGS__)
+#define ERROR(format, ...) LOG("ERROR", format, __VA_ARGS__)
 
 #endif
