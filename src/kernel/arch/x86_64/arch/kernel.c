7efea1b2a774a3a39545c5cfd30ca368bdf5f98c
William Durand <will+git@drnd.me>
2021-11-17 09:43:13 +0100

    kernel: add a comment for the 'hlt' instruction

--- src/kernel/arch/x86_64/arch/kernel.c
+++ src/kernel/arch/x86_64/arch/kernel.c
@@ -9,6 +9,8 @@ void arch_halt_system()
   isr_disable_interrupts();
 
   while (1) {
+    // This allows the CPU to enter a sleep state in which it consumes much less
+    // energy. See: https://en.wikipedia.org/wiki/HLT_(x86_instruction)
     __asm__("hlt");
   }
 }
