2eb78b1e119d0be3f2f96765440a439f0413ac1c
William Durand <will+git@drnd.me>
2021-11-21 15:37:04 +0100

    kernel: kshell: do not power off system when kshell receives arguments (#377)

--- src/kernel/kshell/kshell.c
+++ src/kernel/kshell/kshell.c
@@ -129,12 +129,11 @@ void kshell(int argc, char* argv[])
     for (int i = 1; i < argc; i++) {
       if (strcmp(argv[i], "selftest") == 0) {
         selftest();
+        power_off();
       } else {
-        printf("unsupported command: %s\n", argv[i]);
+        printf("kshell: unsupported argument: %s\n", argv[i]);
       }
     }
-
-    power_off();
   }
 
   printf("\nThis is " KERNEL_NAME " kernel shell. "
