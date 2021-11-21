3c52b524233d86d04c09feb9b62c648722d27238
William Durand <will+git@drnd.me>
2021-11-21 22:45:51 +0100

    kernel: fs: proc: refactor procfs to be arch-independent

---
+++ src/kernel/fs/proc.c
@@ -0,0 +1,87 @@
+#include <fs/proc.h>
+
+#include <arvern/utils.h>
+#include <fs/logging.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+uint64_t proc_read(inode_t inode, void* buffer, uint64_t size, uint64_t offset);
+
+static vfs_driver_t proc_driver = {
+  NULL,      // open
+  NULL,      // close
+  proc_read, // read
+  NULL,      // write
+  NULL,      // stat
+  NULL,      // isatty
+  NULL,      // readdir
+  NULL,      // finddir
+  NULL,      // cleanup
+  NULL,      // create
+};
+
+bool proc_fs_init()
+{
+  return (bool)vfs_mount(FS_PROC_MOUNTPOINT, proc_fs_create());
+}
+
+inode_t proc_fs_create()
+{
+  inode_t node = vfs_make_directory("proc");
+  node->driver = &proc_driver;
+
+  return node;
+}
+
+inode_t proc_fs_create_readonly_file(const char* name, proc_read_t* read_fn)
+{
+  inode_t node = calloc(1, sizeof(vfs_node_t));
+
+  strcpy(node->name, name);
+  node->type = FS_FILE;
+  node->data = (uintptr_t)read_fn;
+  // TODO: we probably want a specific driver for read-only pseudo-files.
+  node->driver = &proc_driver;
+
+  return node;
+}
+
+uint64_t proc_read(inode_t inode, void* buffer, uint64_t size, uint64_t offset)
+{
+  if (vfs_type(inode) != FS_FILE) {
+    return 0;
+  }
+
+  // Empty buffer.
+  strcpy(buffer, "");
+
+  if (inode->data) {
+    char buf[256];
+
+    if (((proc_read_t*)(inode->data))(buf, 256) != 0) {
+      return 0;
+    }
+
+    uint64_t len = strlen(buf);
+
+    if (offset > len) {
+      return 0;
+    }
+
+    if (size > len) {
+      size = len;
+    }
+
+    if (offset + size > len) {
+      size = len - offset;
+    }
+
+    memcpy(buffer, (char*)buf + offset, size);
+    ((char*)buffer)[size] = '\0';
+
+    return size;
+  }
+
+  return 0;
+}
--- tests/kernel/fs/proc.c
+++ tests/kernel/fs/proc.c
@@ -7,34 +7,11 @@
 
 #define BUF_SIZE 20
 
-// This is a fake implementation.
-uint64_t timer_uptime()
+int proc_read_version(char* buf, size_t size)
 {
-  return 123;
-}
+  snprintf(buf, size, "some version\n");
 
-// This is a fake implementation.
-uint64_t frame_get_used_count()
-{
-  return 1;
-}
-
-// This is a fake implementation.
-uint64_t frame_get_max_count()
-{
-  return 123;
-}
-
-// This is a fake implementation.
-uint64_t alloc_get_used_count()
-{
-  return 4;
-}
-
-// This is a fake implementation.
-uint64_t alloc_get_max_count()
-{
-  return 567;
+  return 0;
 }
 
 int main()
@@ -43,12 +20,22 @@ int main()
 
   char buf[BUF_SIZE];
 
-  inode_t root, uptime, hostname;
+  inode_t root, version;
 
   describe("proc_fs_create()");
   root = vfs_mount("/", proc_fs_create());
   assert(root != 0, "can be mounted");
   assert(vfs_type(root) == FS_DIRECTORY, "is a directory");
+  end_describe();
+
+  describe("proc_isatty()");
+  assert(vfs_isatty(root) == 0, "returns 0");
+  end_describe();
+
+  describe("proc_fs_create_readonly_file()");
+  // Add a pseudo-file that will be used by the next test cases.
+  vfs_mount("/version",
+            proc_fs_create_readonly_file("version", &proc_read_version));
   end_describe();
 
   describe("proc_readdir()");
@@ -64,23 +51,8 @@ int main()
   free(de);
 
   de = vfs_readdir(root, 2);
-  assert(strcmp(de->name, "hostname") == 0,
-         "returns the hostname file in position #2");
-  free(de);
-
-  de = vfs_readdir(root, 3);
-  assert(strcmp(de->name, "meminfo") == 0,
-         "returns the meminfo file in position #3");
-  free(de);
-
-  de = vfs_readdir(root, 4);
-  assert(strcmp(de->name, "uptime") == 0,
-         "returns the uptime file in position #4");
-  free(de);
-
-  de = vfs_readdir(root, 5);
   assert(strcmp(de->name, "version") == 0,
-         "returns the version file in position #5");
+         "returns the version file in position #2");
   free(de);
   end_describe();
 
@@ -88,63 +60,29 @@ int main()
   inode_t not_found = vfs_namei("/file-not-found");
   assert(not_found == NULL, "returns NULL when file is not found");
 
-  uptime = vfs_namei("/uptime");
-  assert(vfs_type(uptime) == FS_FILE, "finds the uptime file");
+  version = vfs_namei("/version");
+  assert(vfs_type(version) == FS_FILE, "finds the version file");
 
-  inode_t not_uptime = vfs_namei("/uptimee");
-  assert(not_uptime == NULL, "returns NULL when file is not found");
-  end_describe();
-
-  describe("proc_isatty()");
-  assert(vfs_isatty(root) == 0, "returns 0");
+  inode_t not_version = vfs_namei("/versionn");
+  assert(not_version == NULL, "returns NULL when file is not found");
   end_describe();
 
   describe("proc_read()");
-  uptime = vfs_namei("/uptime");
-  uint64_t bytes_read = vfs_read(uptime, buf, BUF_SIZE, 0);
-  assert(strcmp(buf, "123\n") == 0, "reads the content of the uptime file");
-  assert(bytes_read == 4, "returns the number of bytes read");
+  version = vfs_namei("/version");
+  uint64_t bytes_read = vfs_read(version, buf, BUF_SIZE, 0);
+  assert(strcmp(buf, "some version\n") == 0,
+         "reads the content of the version file");
+  assert(bytes_read == 13, "returns the number of bytes read");
 
-  bytes_read = vfs_read(uptime, buf, BUF_SIZE, 1);
-  assert(strcmp(buf, "23\n") == 0,
-         "reads the content of the uptime file with offset");
-  assert(bytes_read == 3, "returns the number of bytes read with offset");
+  bytes_read = vfs_read(version, buf, BUF_SIZE, 1);
+  assert(strcmp(buf, "ome version\n") == 0,
+         "reads the content of the version file with offset");
+  assert(bytes_read == 12, "returns the number of bytes read with offset");
 
-  bytes_read = vfs_read(uptime, buf, BUF_SIZE, 4);
+  bytes_read = vfs_read(version, buf, BUF_SIZE, 30);
   assert(strcmp(buf, "") == 0, "handles big offsets");
   assert(bytes_read == 0, "returns no bytes read when offset is too big");
   end_describe();
-
-  describe("proc_stat()");
-  vfs_stat_t stat = { 0 };
-  vfs_stat(root, &stat);
-  assert(stat.size == 4, "sets a size equal to the number of files");
-
-  vfs_stat(vfs_namei("/uptime"), &stat);
-  assert(stat.size == 0, "sets the size to 0 by default");
-
-  vfs_stat(vfs_namei("/hostname"), &stat);
-  assert(stat.size > 0, "sets the correct size for hostname");
-  end_describe();
-
-  describe("proc_write()");
-  char* new_hostname = "new-hostname";
-  hostname = vfs_namei("/hostname");
-  uptime = vfs_namei("/uptime");
-  uint64_t bytes_written =
-    vfs_write(hostname, new_hostname, strlen(new_hostname), 0);
-  vfs_read(hostname, buf, BUF_SIZE, 0);
-  // Remove the trailing newline.
-  buf[strlen(buf) - 1] = '\0';
-  assert(strcmp(buf, new_hostname) == 0, "writes the new hostname value");
-  assert(bytes_written == strlen(new_hostname),
-         "returns the number of bytes written");
-  char* long_hostname = "this-is-a-super-looooooong-host-name";
-  bytes_written = vfs_write(hostname, long_hostname, strlen(long_hostname), 0);
-  assert(bytes_written == strlen(long_hostname),
-         "reallocs memory to deal with long hostname values");
-  bytes_written = vfs_write(uptime, new_hostname, strlen(new_hostname), 0);
-  assert(bytes_written == 0, "does not allow write to uptime");
 
   vfs_free(root);
 
