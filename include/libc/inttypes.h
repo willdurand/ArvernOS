efe816f1d4426a9d377d54b82bb17d2a852ce22b
William Durand <will+git@drnd.me>
2021-11-29 09:34:55 +0100

    build: use clang standard headers instead of our owns
    The C language defines several standard headers that can be used in a freestanding environment. Let's use the ones provided by the compiler instead of having our owns to ease our job. Those headers define architecture related types, etc.

--- include/libc/inttypes.h
+++ include/libc/inttypes.h
@@ -1,20 +1,26 @@
 #ifndef INTTYPES_H
 #define INTTYPES_H
 
+#ifdef __arm__
+#define PRI_PREFIX "l"
+#else
+#define PRI_PREFIX "ll"
+#endif
+
 #ifndef PRIi64
-#define PRIi64 "lli"
+#define PRIi64 PRI_PREFIX "i"
 #endif
 
 #ifndef PRIu64
-#define PRIu64 "llu"
+#define PRIu64 PRI_PREFIX "u"
 #endif
 
 #ifndef PRIx64
-#define PRIx64 "llx"
+#define PRIx64 PRI_PREFIX "x"
 #endif
 
 #ifndef PRId64
-#define PRId64 "lld"
+#define PRId64 PRI_PREFIX "d"
 #endif
 
 #endif
