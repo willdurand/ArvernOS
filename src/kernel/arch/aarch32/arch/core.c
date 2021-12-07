42c9d51cb591dfc3887eb3f20073f5164fe6608c
William Durand <will+git@drnd.me>
2021-12-07 21:28:18 +0100

    kernel: aarch32: add core/isr stubs

---
+++ src/kernel/arch/aarch32/arch/core.c
@@ -0,0 +1,16 @@
+#include <arch/core.h>
+
+void arch_isr_init()
+{
+  // TODO: Implement this function for aarch32.
+}
+
+void arch_isr_enable_interrupts()
+{
+  // TODO: Implement this function for aarch32.
+}
+
+void arch_isr_disable_interrupts()
+{
+  // TODO: Implement this function for aarch32.
+}
