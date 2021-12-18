4d15f831211429574d04ac8933bc3809d7b2cb34
William Durand <will+git@drnd.me>
2021-12-18 19:42:54 +0100

    kernel: aarch64: raspi3: use bcm2835_timer driver (#516)

--- src/kernel/arch/aarch64/board/raspi3/board.h
+++ src/kernel/arch/aarch64/board/raspi3/board.h
@@ -7,15 +7,7 @@
 #define GPIO_BASE (MMIO_BASE + 0x200000)
 
 // interrupts
-#define IRQ_PENDING_1      (MMIO_BASE + 0x0000B204)
-#define ENABLE_IRQS_1      (MMIO_BASE + 0x0000B210)
-#define SYSTEM_TIMER_IRQ_1 (1 << 1)
-
-// timer
-#define TIMER_CS    (MMIO_BASE + 0x00003000)
-#define TIMER_CLO   (MMIO_BASE + 0x00003004)
-#define TIMER_CHI   (MMIO_BASE + 0x00003008)
-#define TIMER_C1    (MMIO_BASE + 0x00003010)
-#define TIMER_CS_M1 (1 << 1)
+#define IRQ_PENDING_1 (MMIO_BASE + 0x0000B204)
+#define ENABLE_IRQS_1 (MMIO_BASE + 0x0000B210)
 
 #endif
