ea5ba117478fdc55ae39cb4a2985ac2298fcbb6a
William Durand <will+git@drnd.me>
2021-12-18 14:35:34 +0100

    kernel: aarch64: fix kmain() declaration (#513)

--- src/kernel/arch/aarch64/kmain.h
+++ src/kernel/arch/aarch64/kmain.h
@@ -2,9 +2,11 @@
 #ifndef AARCH64_KMAIN_H
 #define AARCH64_KMAIN_H
 
+#include <stdint.h>
+
 /**
  * This is the entrypoint of the kernel (C code).
  */
-void kmain() __asm__("kmain");
+void kmain(uintptr_t w0) __asm__("kmain");
 
 #endif
