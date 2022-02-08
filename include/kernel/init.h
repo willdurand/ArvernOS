9a41a76178c106c2946fc6fb4e5d7b9b62753c79
William Durand <will+git@drnd.me>
2022-02-08 21:47:06 +0100

    kernel: fix TODO by using attribute (used) in init.h

--- include/kernel/init.h
+++ include/kernel/init.h
@@ -12,11 +12,9 @@ typedef int (*initcall_t)(void);
 extern unsigned char __initcall_start;
 extern unsigned char __initcall_end;
 
-// TODO: We should ideally declare these declarations `static`, however LLVM
-// strips them out because they are unused and despite `unused` being set.
 #define __initcall(func)                                                       \
-  initcall_t __initcall_##func                                                 \
-    __attribute__((unused, __section__(".initcall_functions"))) = func
+  static initcall_t __initcall_##func                                          \
+    __attribute__((used, __section__(".initcall_functions"))) = func
 
 /**
  * Registers an initcall function that will be loaded after the
