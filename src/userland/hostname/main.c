2be37260c9502990b840b29afe82db1540379274
William Durand <will+git@drnd.me>
2022-02-12 18:56:54 +0100

    userland: hostname: handle open() errors (#560)

--- src/userland/hostname/main.c
+++ src/userland/hostname/main.c
@@ -15,20 +15,25 @@ int main(int argc, char* argv[])
   int retval = 0;
   int fd = open(HOSTNAME_FILE, O_RDWR);
 
+  if (fd < 0) {
+    printf("could not open %s (errno=%d)\n", HOSTNAME_FILE, errno);
+    return 1;
+  }
+
   if (argc == 1) {
     char buf[50];
     if (read(fd, buf, 50) > 0) {
       printf("%s", buf);
     } else {
       printf("failed to read from %s (errno=%d)\n", HOSTNAME_FILE, errno);
-      retval = 1;
+      retval = 2;
     }
   } else {
     if (write(fd, argv[1], strlen(argv[1])) > 0) {
       printf("hostname updated\n");
     } else {
       printf("failed to write to %s (errno=%d)\n", HOSTNAME_FILE, errno);
-      retval = 2;
+      retval = 3;
     }
   }
 
