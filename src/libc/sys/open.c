b3c7327034109cae6f9c6f7cbf32f24a3a667222
William Durand <will+git@drnd.me>
2022-02-12 21:57:31 +0100

    libc: sys: add aarch64 compat in open()

--- src/libc/sys/open.c
+++ src/libc/sys/open.c
@@ -1,11 +1,19 @@
 #include <sys/syscall.h>
 
+#include <fcntl.h>
+
 int open(const char* pathname, uint32_t flags)
 {
 #ifdef __is_libk
   return k_open(pathname, flags);
 #else
-  int retval = syscall(SYSCALL_OPEN, pathname, flags);
+  int retval;
+
+#ifdef __aarch64__
+  retval = syscall(SYSCALL_OPENAT, AT_FDCWD, pathname, flags);
+#else
+  retval = syscall(SYSCALL_OPEN, pathname, flags);
+#endif
 
   SYSCALL_SET_ERRNO();
 
