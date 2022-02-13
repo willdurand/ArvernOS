ae748b578ce4ab90b272a0f92e855df26f1af19b
William Durand <will+git@drnd.me>
2022-02-13 22:11:55 +0100

    kernel: x86_64: save and restore registers in syscall_handler

--- src/kernel/arch/x86_64/asm/k_syscall.asm
+++ src/kernel/arch/x86_64/asm/k_syscall.asm
@@ -1,15 +1,42 @@
 global syscall_handler
 
 syscall_handler:
+  sti
+
   push rcx     ; save the return address
   push r11     ; save the flags
+  push rbp
+  push rbx
+  push rdx
+  push rdi
+  push rsi
+  push r8
+  push r9
+  push r10
+  push r12
+  push r13
+  push r14
+  push r15
+
   mov rcx, r10 ; fix 3rd syscall arg
-  sti
 
   extern syscall_handlers
   call [rax * 8 + syscall_handlers]
 
-  cli
+  pop r15
+  pop r14
+  pop r13
+  pop r12
+  pop r10
+  pop r9
+  pop r8
+  pop rsi
+  pop rdi
+  pop rdx
+  pop rbx
+  pop rbp
   pop r11
   pop rcx
+
+  cli
   o64 sysret
