060dc3c17a4b46aefbb23434662d33f85575d7a0
William Durand <will+git@drnd.me>
2022-02-13 22:21:50 +0100

    libc: crt0: only pass main, argc and argv to start_main() (#569)

--- src/libc/asm/x86_64/crt0.asm
+++ src/libc/asm/x86_64/crt0.asm
@@ -6,9 +6,8 @@ section .text
 _start:
   ; Prepare the arguments for `start_main()`.
   mov rdi, main ; `main()`
-  pop rsi       ; `argc`
-  pop rdx       ; `argv`
-  pop rcx       ; `envp`
+  pop rsi       ; argc
+  mov rdx, rsp  ; argv
 
   extern start_main
   call start_main
