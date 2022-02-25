7cbdfe5b726ccf5f8cbe155b1e7438c03a4a49d2
William Durand <will+git@drnd.me>
2022-02-25 10:52:50 +0100

    libk: add assert() implementation (#598)

--- include/libc/assert.h
+++ include/libc/assert.h
@@ -2,7 +2,26 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
+#include <panic.h>
+
+#ifdef __is_libk
+
+#define assert(expr)                                                           \
+  do {                                                                         \
+    if (!(expr)) {                                                             \
+      PANIC("Assertion failed at %s:%d:%s(): %s",                              \
+            __FILE__,                                                          \
+            __LINE__,                                                          \
+            __func__,                                                          \
+            #expr);                                                            \
+    }                                                                          \
+  } while (0)
+
+#else // __is_libk
+
 #define assert(ignore) ((void)0)
+
+#endif // __is_libk
 
 #define static_assert _Static_assert
 
