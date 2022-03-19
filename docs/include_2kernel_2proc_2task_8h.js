063cb613eac62ae9b6e62a5e56724cf965306486
William Durand <will+git@drnd.me>
2022-03-19 21:58:40 +0100

    docs: sync with master

--- docs/include_2kernel_2proc_2task_8h.js
+++ docs/include_2kernel_2proc_2task_8h.js
@@ -6,6 +6,7 @@ var include_2kernel_2proc_2task_8h =
     [ "task_exit", "include_2kernel_2proc_2task_8h.html#ae4bc8886787eac6213c6cd0eab9b2893", null ],
     [ "task_get_current", "include_2kernel_2proc_2task_8h.html#a32c1128364ef08b15edabdac124bc11b", null ],
     [ "task_init", "include_2kernel_2proc_2task_8h.html#a0dd765564611ce3bf2bd466fe2ca4ae5", null ],
+    [ "task_print_info", "include_2kernel_2proc_2task_8h.html#aba62108350393ac5cb4f2cf0f88c2a22", null ],
     [ "task_schedule", "include_2kernel_2proc_2task_8h.html#a7b77adca1da85233e213a9a0eacd8086", null ],
     [ "task_schedule_on_timer_tick", "include_2kernel_2proc_2task_8h.html#ac196c54c15d40be45138ffdbfb42758c", null ]
 ];
--- docs/search/functions_11.js
+++ docs/search/functions_11.js
@@ -5,12 +5,13 @@ var searchData=
   ['task_5fexit_2',['task_exit',['../include_2kernel_2proc_2task_8h.html#ae4bc8886787eac6213c6cd0eab9b2893',1,'task.c']]],
   ['task_5fget_5fcurrent_3',['task_get_current',['../include_2kernel_2proc_2task_8h.html#a32c1128364ef08b15edabdac124bc11b',1,'task.c']]],
   ['task_5finit_4',['task_init',['../include_2kernel_2proc_2task_8h.html#a0dd765564611ce3bf2bd466fe2ca4ae5',1,'task.h']]],
-  ['task_5fschedule_5',['task_schedule',['../include_2kernel_2proc_2task_8h.html#a7b77adca1da85233e213a9a0eacd8086',1,'task.c']]],
-  ['task_5fschedule_5fon_5ftimer_5ftick_6',['task_schedule_on_timer_tick',['../include_2kernel_2proc_2task_8h.html#ac196c54c15d40be45138ffdbfb42758c',1,'task.c']]],
-  ['test_7',['test',['../syscall_8h.html#a83e57b3c4cec0806723c6ddf9a6e4a00',1,'test.c']]],
-  ['timer_5finit_8',['timer_init',['../timer_8h.html#a896a4c37c6ecf8868ef0dc758e5a598c',1,'timer.c']]],
-  ['timer_5fuptime_9',['timer_uptime',['../timer_8h.html#ad5df37e1f85cabd9c5d43aa1dfc0e37e',1,'timer.c']]],
-  ['timer_5fuptime_5fmicroseconds_10',['timer_uptime_microseconds',['../timer_8h.html#a9e237401e78bbceb0c0070ef2ecb4569',1,'timer.c']]],
-  ['translate_11',['translate',['../paging_8h.html#a1f069c46ac4245771221fc7f0bcfb2e0',1,'paging.c']]],
-  ['translate_5fpage_12',['translate_page',['../paging_8h.html#a93e89a9ca57751d1b04f8d95ccbddb4b',1,'paging.c']]]
+  ['task_5fprint_5finfo_5',['task_print_info',['../include_2kernel_2proc_2task_8h.html#aba62108350393ac5cb4f2cf0f88c2a22',1,'task.c']]],
+  ['task_5fschedule_6',['task_schedule',['../include_2kernel_2proc_2task_8h.html#a7b77adca1da85233e213a9a0eacd8086',1,'task.c']]],
+  ['task_5fschedule_5fon_5ftimer_5ftick_7',['task_schedule_on_timer_tick',['../include_2kernel_2proc_2task_8h.html#ac196c54c15d40be45138ffdbfb42758c',1,'task.c']]],
+  ['test_8',['test',['../syscall_8h.html#a83e57b3c4cec0806723c6ddf9a6e4a00',1,'test.c']]],
+  ['timer_5finit_9',['timer_init',['../timer_8h.html#a896a4c37c6ecf8868ef0dc758e5a598c',1,'timer.c']]],
+  ['timer_5fuptime_10',['timer_uptime',['../timer_8h.html#ad5df37e1f85cabd9c5d43aa1dfc0e37e',1,'timer.c']]],
+  ['timer_5fuptime_5fmicroseconds_11',['timer_uptime_microseconds',['../timer_8h.html#a9e237401e78bbceb0c0070ef2ecb4569',1,'timer.c']]],
+  ['translate_12',['translate',['../paging_8h.html#a1f069c46ac4245771221fc7f0bcfb2e0',1,'paging.c']]],
+  ['translate_5fpage_13',['translate_page',['../paging_8h.html#a93e89a9ca57751d1b04f8d95ccbddb4b',1,'paging.c']]]
 ];
