982ceed2127a72e44e40385a612df289d5fffc89
William Durand <will+git@drnd.me>
2022-04-04 20:56:58 +0200

    kernel: core: move ELF loader to shared code
    This is not quite correct for non-x86_64 architectures but that's a start at least...

--- include/kernel/core/elf.h
+++ include/kernel/core/elf.h
@@ -1,6 +1,10 @@
 /**
  * @file
  * @see https://www.uclibc.org/docs/elf-64-gen.pdf
+ * @see https://man7.org/linux/man-pages/man5/elf.5.html
+ *
+ * This header file contains the definitions of the Executable and Linking
+ * Format (ELF) for both 32 (arm) and 64 bits (the default).
  */
 #ifndef CORE_ELF_H
 #define CORE_ELF_H
@@ -34,15 +38,29 @@
 /// Identifies a section that occupies memory during process execution.
 #define ELF_SECTION_FLAG_ALLOC 0x2
 
+#ifdef __arm__
+
+#define ElfN_Addr uint32_t
+#define ElfN_Off  uint32_t
+#define ElfN_Size uint32_t
+
+#else // __arm__
+
+#define ElfN_Addr uint64_t
+#define ElfN_Off  uint64_t
+#define ElfN_Size uint64_t
+
+#endif
+
 typedef struct elf_header
 {
   unsigned char identity[16];
   uint16_t type;
   uint16_t machine;
   uint32_t version;
-  uint64_t entry;
-  uint64_t ph_offset;
-  uint64_t sh_offset;
+  ElfN_Addr entry;
+  ElfN_Off ph_offset;
+  ElfN_Off sh_offset;
   uint32_t flags;
   uint16_t header_size;
   uint16_t ph_size;
@@ -55,27 +73,32 @@ typedef struct elf_header
 typedef struct elf_program_header
 {
   uint32_t type;
+#ifndef __arm__
   uint32_t flags;
-  uint64_t offset;
-  uint64_t virtual_address;
-  uint64_t physical_address;
-  uint64_t file_size;
-  uint64_t mem_size;
-  uint64_t align;
+#endif
+  ElfN_Off offset;
+  ElfN_Addr virtual_address;
+  ElfN_Addr physical_address;
+  ElfN_Size file_size;
+  ElfN_Size mem_size;
+#ifdef __arm__
+  ElfN_Size flags;
+#endif
+  ElfN_Size align;
 } __attribute__((packed)) elf_program_header_t;
 
 typedef struct elf_section_header
 {
-  uint32_t name;            ///< Section name
-  uint32_t type;            ///< Section type
-  uint64_t flags;           ///< Section attributes
-  uint64_t virtual_address; ///< Virtual address in memory
-  uint64_t offset;          ///< Offset in file
-  uint64_t size;            ///< Size of section
-  uint32_t link;            ///< Link to other section
-  uint32_t info;            ///< Miscellaneous information
-  uint64_t addralign;       ///< Address alignment boundary
-  uint64_t entsize;         ///< Size of entries, if section has table
+  uint32_t name;             ///< Section name
+  uint32_t type;             ///< Section type
+  ElfN_Size flags;           ///< Section attributes
+  ElfN_Addr virtual_address; ///< Virtual address in memory
+  ElfN_Off offset;           ///< Offset in file
+  ElfN_Size size;            ///< Size of section
+  uint32_t link;             ///< Link to other section
+  uint32_t info;             ///< Miscellaneous information
+  ElfN_Size addralign;       ///< Address alignment boundary
+  ElfN_Size entsize;         ///< Size of entries, if section has table
 } __attribute__((packed)) elf_section_header_t;
 
 elf_header_t* elf_load(uint8_t* data);
