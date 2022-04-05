0d6ae881160e3cbe67a88506e17a7d6b6470cca6
William Durand <will+git@drnd.me>
2022-04-05 22:04:11 +0200

    kernel: sys: use the right logger in k_exit() (#632)

--- src/kernel/sys/k_exit.c
+++ src/kernel/sys/k_exit.c
@@ -1,10 +1,10 @@
 #include <sys/k_syscall.h>
 
-#include <proc/logging.h>
 #include <proc/task.h>
+#include <sys/logging.h>
 
 void k_exit(int code)
 {
-  PROC_DEBUG("task id=%d is exiting (code=%d)", CURRENT_TASK->id, code);
+  SYS_DEBUG("task id=%d is exiting (code=%d)", CURRENT_TASK->id, code);
   task_exit(code);
 }
